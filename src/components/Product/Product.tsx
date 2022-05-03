e7bd7eb25e040496c5d112b29f6624d79807f563
DevEnock <enockomondi305@gmail.com>
2022-05-03 12:55:05 +0300

    styling product-list

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -3,10 +3,14 @@ import React from 'react'
 function Product(props: any) {
   return (
     <div className='product'>
-      <img src={props.img} alt="img" />
-      <p>{props.title}</p>
-      <h3>${props.price}</h3>
-      <p>{props.category}</p>
+      <div className="card">
+        <img src={props.img} alt="img" />
+        <div className="card-body">
+          <h3 className="card-title">{props.title}</h3>
+          <h1 className='card-title'>${props.price}</h1>
+          <p className='card-text'>{props.category}</p>
+        </div>
+      </div>
     </div>
   )
 }
